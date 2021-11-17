fe6f5e3e8fc534c2e429bcdedd1b948a62358ebe
Antoine Bollinger <antoine.bollinger@gmail.com>
2021-11-17 13:58:05 +0100

    first adaptations

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,104 @@
 // Scripts
 // 
 
+// Cookies constent baner 
+
+const cookieBanner = document.querySelector('.cookie-banner');
+const closeCookieBanner = document.querySelector('.cookie-close');
+if (localStorage.getItem("cookieSeen") !== "shown")
+    cookieBanner.style.display = 'block'; localStorage.setItem("cookieSeen", "shown");
+closeCookieBanner.addEventListener('click', () => {
+    cookieBanner.style.display = 'none';
+});
+
+// Reveal sections
+const allSections = document.querySelectorAll('section');
+
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
+
+// Skils
+
+const skillsContainer = document.getElementById('skills').querySelector('.row');
+const skillsModalsContainer = document.getElementById('skills-modals');
+const mySkills = [
+    {
+        "img": "01.jpg",
+        "lazy-img": "01_small.jpg",
+        "title": "Se développer",
+        "texte": "Apprendre à se développer"
+    }
+];
+
+
+mySkills.forEach((ele, index) => {
+    skillsContainer.insertAdjacentHTML('beforeend', `
+        <div class="col-lg-4 col-sm-6 mb-4">
+            <!-- Portfolio item ${1 + index}-->
+            <div class="portfolio-item">
+                <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal${1 + index}">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid lazy-img" src="assets/img/portfolio/${ele.img}" alt="${ele.title}" />
+                </a>
+                <div class="portfolio-caption">
+                    <div class="portfolio-caption-heading">${ele.title}</div>
+                    <!-- <div class="portfolio-caption-subheading text-muted">Illustration</div> -->
+                </div>
+            </div>
+        </div>
+    `);
+    skillsModalsContainer.insertAdjacentHTML('beforeend', `
+        <div class="portfolio-modal modal fade" id="portfolioModal${1 + index}" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg"
+                            alt="Fermer" /></div>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-8">
+                                <div class="modal-body">
+                                    <!-- Project details-->
+                                    <h2 class="text-uppercase">${ele.title}</h2>
+                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                                    <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/${ele.img}" alt="${ele.title}" />
+                                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
+                                        repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
+                                        nostrum, reiciendis facere nemo!</p>
+                                  
+                                    <!-- <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
+                                        type="button">
+                                        <i class="fas fa-times me-1"></i>
+                                        Close Project
+                                    </button> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    `);
+});
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -52,3 +150,29 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', function () {
+        entry.target.classList.remove('lazy-img');
+    });
+
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
