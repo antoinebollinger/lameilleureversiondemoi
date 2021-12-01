aeba5051082f1cf289722febbd0697bd1949aa9d
Antoine Bollinger <antoine.bollinger@gmail.com>
2021-12-01 12:02:44 +0100

    programs texts

--- src/js/app.js
+++ src/js/app.js
@@ -127,36 +127,34 @@ export default class App {
                             <!-- <div class="portfolio-caption-subheading text-muted">Illustration</div> -->
                         </div>
                     </div>
-                </div>
-            `);
-            this.#skillsModalsContainer.insertAdjacentHTML('beforeend', `
-                <div class="portfolio-modal modal fade" id="portfolioModal${1 + index}" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg"
-                                    alt="Fermer" /></div>
-                            <div class="container">
-                                <div class="row justify-content-center">
-                                    <div class="col-lg-8">
-                                        <div class="modal-body">
-                                            <!-- Project details-->
-                                            <h2 class="text-uppercase">${ele.title}</h2>
-                                            <!-- <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p> -->
-                                            <img class="img-fluid d-block mx-auto" src="assets/img/expertise/${ele.img}" alt="${ele.title}" />
-                                            <p>${ele.text}</p>
-                                        
-                                            <!-- <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
-                                                type="button">
-                                                <i class="fas fa-times me-1"></i>
-                                                Close Project
-                                            </button> -->
+                    <div class="portfolio-modal modal fade" id="portfolioModal${1 + index}" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg"
+                                        alt="Fermer" /></div>
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-lg-8">
+                                            <div class="modal-body">
+                                                <!-- Project details-->
+                                                <h2 class="text-uppercase">${ele.title}</h2>
+                                                <!-- <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p> -->
+                                                <img class="img-fluid d-block mx-auto" src="assets/img/expertise/${ele.img}" alt="${ele.title}" />
+                                                <p>${ele.text}</p>
+                                            
+                                                <!-- <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
+                                                    type="button">
+                                                    <i class="fas fa-times me-1"></i>
+                                                    Close Project
+                                                </button> -->
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>    
+                    </div>    
+                </div>
             `);
         });
     }
