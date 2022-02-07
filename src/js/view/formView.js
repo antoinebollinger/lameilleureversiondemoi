7d9691ea995ddbceb7b06a5d64333abca2da56fd
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-02-07 09:39:09 +0100

    logo changes

--- src/js/view/formView.js
+++ src/js/view/formView.js
@@ -156,7 +156,7 @@ export default class Form extends View {
                         </div>
                     </form>
                     <div class="modal-footer">
-                    <img src="assets/img/logos/LMVDM-5-blanc.svg" class="w-25 d-none d-lg-block" />
+                    <img src="assets/img/logos/v4/whiteText/purpleLeaf/LMVDM.svg" class="w-25 d-none d-lg-block" />
                     <button type="button" class="btn btn-lg btn-primary">Démarrer</button>
                   </div>
                 </div>
